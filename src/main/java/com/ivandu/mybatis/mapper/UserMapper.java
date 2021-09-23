bad21424dc5100844b787940bb4fe2b1bf6d6063
ivandu <mrivandu@hotmail.com>
2021-09-24 02:15:31 +0800

    初次提交

---
+++ src/main/java/com/ivandu/mybatis/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.ivandu.mybatis.mapper;
+
+import com.ivandu.mybatis.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    int insert(User user) throws Exception;
+    int update(User user) throws Exception;
+    int deleteUser(Integer id) throws Exception;
+    User selectUserById(Integer id) throws Exception;
+    List<User> selectAllUsers() throws Exception;
+}
