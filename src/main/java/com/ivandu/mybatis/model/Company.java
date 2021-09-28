854d5aa64df5f477cc24c7d26151acb0f491d01f
ivandu <mrivandu@hotmail.com>
2021-09-29 02:01:23 +0800

    一对一：2.0.0

--- src/main/java/com/ivandu/mybatis/model/Company.java
+++ src/main/java/com/ivandu/mybatis/model/Company.java
@@ -4,15 +4,36 @@ public class Company {
     private Integer id;
     private String name;
     private CEO ceo;
-    public Company() {
 
+    public Company(){}
+
+    public Company(Integer id, String name, CEO ceo) {
+        this.id = id;
+        this.name = name;
+        this.ceo = ceo;
     }
 
-    public void setId(Integer id){
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public Integer getId(){
+    public Integer getId() {
         return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CEO getCeo() {
+        return ceo;
+    }
+
+    public void setCeo(CEO ceo) {
+        this.ceo = ceo;
     }
 }
